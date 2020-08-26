acf2ae4bea4b63259456be971a29dc94ad1a5d0d
Md Emran Nazir <mdemrannazir9999@gmail.com>
2020-08-26 21:50:30 +0600

    Updates

---
+++ precache-manifest.c43e809b77ffcf024e3b0f1baffbe0ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0413b35294dedda437dbaffe1cc48493",
+    "url": "/app-covid-19/index.html"
+  },
+  {
+    "revision": "812ab7e440f60c3a10de",
+    "url": "/app-covid-19/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "aa24b68e924163a1c1fd",
+    "url": "/app-covid-19/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "812ab7e440f60c3a10de",
+    "url": "/app-covid-19/static/js/2.36f7e606.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/app-covid-19/static/js/2.36f7e606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa24b68e924163a1c1fd",
+    "url": "/app-covid-19/static/js/main.83f29771.chunk.js"
+  },
+  {
+    "revision": "6803c591619a223b02c8",
+    "url": "/app-covid-19/static/js/runtime-main.4ef13493.js"
+  }
+]);
